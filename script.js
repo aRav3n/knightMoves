eb76a69bc2b6b3fdbf2d7922c6a83aa674d99cb8
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-02-13 12:25:29 -0600

    Get all possible knight moves from every square on the board

--- script.js
+++ script.js
@@ -46,6 +46,18 @@ const chessSquare = function (currentPositionArray) {
   return { currentPositionArray, movesFromPosition };
 };
 
+// board = 2D array 8 x 8
+const makeBoard = function () {
+  const board = [];
+  for (let i = 0; i < 8; i++) {
+    board[i] = [];
+    for (let j = 0; j < 8; j++) {
+      board[i][j] = chessSquare([i, j]);
+    }
+  }
+  return board;
+};
+
 // shortestPath() function to find shortest path between current location and desired location
 
 // knightMoves()
