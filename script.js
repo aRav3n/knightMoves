17308d2a543a078661c378dfa7aab74eea50e8de
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-02-13 12:23:39 -0600

    Get array of all possible moves from a given position

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-/*
-// board = 2D array 8 x 8
-const makeBoard = function () {
-  const board = [];
-  for (let i = 0; i < 8; i++) {
-    board[i] = [];
-    for (let j = 0; j < 8; j++) {
-      board[i][j] = chessSquare(i, j);
-    }
-  }
-  console.log(board);
-  return board;
-};
-*/
-
-// possibleMoves()
-const possibleMoves = function (currentPositionArray) {
+// validatePosition()
+const validatePosition = function (currentPositionArray) {
   let validCurrentPosition = true;
   if (
     currentPositionArray[0] < 0 ||
@@ -25,18 +10,41 @@ const possibleMoves = function (currentPositionArray) {
     validCurrentPosition = false;
   }
   if (!validCurrentPosition) {
-    console.log(`starting location ${currentPositionArray} is not valid`);
     return false;
   }
+  return true;
+};
+
+// possibleMoves()
+const possibleMoves = function (currentPositionArray) {
+  validatePosition(currentPositionArray);
+  const possibleMoves = [];
+  const moveStructure = [
+    [1, 2],
+    [2, 1],
+    [1, -2],
+    [2, -1],
+    [-1, 2],
+    [-2, 1],
+    [-1, -2],
+    [-2, -1],
+  ];
+  for (let i = 0; i < moveStructure.length; i++) {
+    const newSquare = [];
+    newSquare.push(moveStructure[i][0] + currentPositionArray[0]);
+    newSquare.push(moveStructure[i][1] + currentPositionArray[1]);
+    if (validatePosition(newSquare)) {
+      possibleMoves.push(newSquare);
+    }
+  }
+  return possibleMoves;
 };
 
 // chessSquare as object factory
-const chessSquare = function (positionAsArray) {
-  const possibleMoves = [];
-  return { positionAsArray, possibleMoves };
+const chessSquare = function (currentPositionArray) {
+  const movesFromPosition = possibleMoves(currentPositionArray);
+  return { currentPositionArray, movesFromPosition };
 };
-
-possibleMoves([1, 2]);
 
 // shortestPath() function to find shortest path between current location and desired location
 
