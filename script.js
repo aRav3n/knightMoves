2d3db55d4bdb43e7a5b083fd0849d2d40d27809a
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-02-13 10:02:02 -0600

    Use array length 2 as input for position

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const chessSquare = function (i, j) {
-  const possibleMoves = [];
-  return { i, j, possibleMoves };
-};
-
+/*
 // board = 2D array 8 x 8
 const makeBoard = function () {
   const board = [];
@@ -15,16 +11,32 @@ const makeBoard = function () {
   console.log(board);
   return board;
 };
+*/
 
 // possibleMoves()
-const possibleMoves = function (i, j) {
-  const gameBoard = makeBoard();
-  if (gameBoard[i][j] === undefined) {
-    console.log(`starting location [${i}][${j}] is not valid`);
+const possibleMoves = function (currentPositionArray) {
+  let validCurrentPosition = true;
+  if (
+    currentPositionArray[0] < 0 ||
+    currentPositionArray[0] > 7 ||
+    currentPositionArray[1] < 0 ||
+    currentPositionArray[1] > 7
+  ) {
+    validCurrentPosition = false;
+  }
+  if (!validCurrentPosition) {
+    console.log(`starting location ${currentPositionArray} is not valid`);
+    return false;
   }
 };
 
-makeBoard();
+// chessSquare as object factory
+const chessSquare = function (positionAsArray) {
+  const possibleMoves = [];
+  return { positionAsArray, possibleMoves };
+};
+
+possibleMoves([1, 2]);
 
 // shortestPath() function to find shortest path between current location and desired location
 
